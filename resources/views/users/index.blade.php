27b84124306167d0d1ae713f28ad15b6c031d2bb
jatakalula <ajatakalula@powertel.co.zw>
2022-07-12 15:36:08 +0200

    second Commit

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,7 +12,10 @@ Users
     <div class="card-header">
         <h3 class="card-title">{{_('Users')}}</h3>
         <div class="card-tools">
-            <a  class="btn btn-primary btn-sm" href="{{ route('users.create') }}"><i class="fas fa-plus-circle"></i>{{_('Create User')}} </a>
+            @can('user-create')
+              <a  class="btn btn-primary btn-sm" href="{{ route('users.create') }}"><i class="fas fa-plus-circle"></i>{{_('Create User')}} </a>  
+            @endcan
+            
         </div>
     </div>
     <!-- /.card-header -->
@@ -41,11 +44,18 @@ Users
                     @endif
                     </td>
                     <td>
-                    <a class="btn btn-info btn-sm" href="{{ route('users.show',$user->id) }}">View</a>
-                    <a class="btn btn-primary btn-sm" href="{{ route('users.edit',$user->id) }}">Edit</a>
-                        {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
-                            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
-                        {!! Form::close() !!}
+                        <form action="{{ route('users.destroy',$user->id) }}" method="POST">
+                            <a class="btn btn-info btn-sm" href="{{ route('users.show',$user->id) }}">View</a>
+                            @can('user-edit')
+                            <a class="btn btn-primary btn-sm" href="{{ route('users.edit',$user->id) }}">Edit</a>   
+                            @endcan
+
+                            @csrf
+                            @method('DELETE')
+                            @can('user-delete')
+                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                            @endcan
+                        </form>
                     </td>
                 </tr>
                 @endforeach
