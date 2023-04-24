5d1ce55720dc9a3c507da2d34d31d11533dad12c
WManyere <wmanyere@powertel.co.zw>
2023-04-24 15:37:00 +0200

    Corrected position-userstatuses DB seeding

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -2,15 +2,15 @@
 
 namespace Database\Seeders;
 
-use App\Models\Position;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Department;
 use App\Models\Section;
+use App\Models\Position;
+use App\Models\UserStatus;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
-use App\Models\UserStatus;
 
 class CreateAdminUserSeeder extends Seeder
 {
@@ -21,7 +21,6 @@ class CreateAdminUserSeeder extends Seeder
      */
     public function run()
     {
-
         $department = Department::create([
             'department' => 'TECHNICAL',
         ]) ;
@@ -30,23 +29,25 @@ class CreateAdminUserSeeder extends Seeder
              'department_id' => $department->id,
 
         ]);
-        $user = User::create([
-            'name' => 'Powertel',
-            'email' => 'admin@powertel.co.zw',
-            'password' => bcrypt('123456')
-        ]);
         $position = Position::create([
             'position'=>'Senior Engineer',
-            'position_id'=>'1',
+            'section_id' => $section->id,
         ]);
-        $userstatus = UserStatus::create([
-            'user_statuses' => 'Unassignable',
-            'user_statuses_id'=>'1',
+        $user_statuses= UserStatus::create([
+            'user_statuses' => 'status_name',
+        ]) ;
+        $user = User::create([
+            'name' => 'Powertel',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123456')
         ]);
+
         $role = Role::create(['name' => 'Admin']);
 
         $permissions = Permission::pluck('id','id')->all();
+
         $role->syncPermissions($permissions);
+
         $user->assignRole([$role->id]);
 
     }
