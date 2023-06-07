adda9654a30b21b512ccd658d82c83a53dd34b9c
P Njanjeni <pnjanjeni@powertel.co.zw>
2023-06-07 13:22:54 +0200

    Implemented toggling on technician myfaults page such that the rectify button does not appear after clearing

--- resources/views/my_faults/index.blade.php
+++ resources/views/my_faults/index.blade.php
@@ -42,28 +42,15 @@ My Faults
                         <strong>{{$fault->description}}</strong> 
                     </td>
                     <td>
-                    <form style="display:inline" action="{{ route('noc-clear.update',$fault->id) }}"  method="POST">  
-                        @csrf
-                        @method('PUT')
-                        @can('noc-clear-faults-clear')
-                        <button type="submit" class="btn btn-sm btn-primary" style="padding:0px 2px; color:#fff;" >Clear</button>   
-                        @endcan
-                    </form>
-                    <form style="display:inline" action="{{ route('chief-tech-clear.update',$fault->id) }}"  method="POST">
-                            @csrf
-                            @method('PUT')
-                            @can('chief-tech-clear-faults-clear')
-                            <button type="submit" class="btn btn-sm btn-primary" style="padding:0px 2px; color:#fff;" >Clear</button>   
-                            @endcan
-                     </form>
+                    <a href="{{ route('faults.show',$fault->id) }}" class="btn btn-sm btn-success" style="padding:0px 2px; color:#fff;" >View</a>
+                        @if ($fault->description==='Fault is under rectification')
                         @can('rectify-fault')
                             <a href="{{ route('rectify.edit',$fault->id) }}" class="btn btn-sm btn-primary" style="padding:0px 2px; color:#fff;" >Rectify</a>
-                        @endcan
+                        @endcan  
                         @can('request-permit')
                         <a href="{{ route('request-permit.edit',$fault->id) }}" class="btn btn-sm btn-warning" style="padding:0px 2px; color:#fff;" >Request Permit</a>                            
-                        @endcan
-                        <a href="{{ route('faults.show',$fault->id) }}" class="btn btn-sm btn-success" style="padding:0px 2px; color:#fff;" >View</a>
-                    
+                        @endcan     
+                        @endif
                     </td>
                 </tr>
                 @endforeach
