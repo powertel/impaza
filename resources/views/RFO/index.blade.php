09fcf1cd39beb2eaab12fa89bb58ddb1a000f17e
jataz <africajataz@gmail.com>
2025-10-12 21:26:26 +0200

    Changes on search and page sizes

--- resources/views/RFO/index.blade.php
+++ resources/views/RFO/index.blade.php
@@ -14,40 +14,53 @@ RFO
             @can('department-create')
             <a  class="btn btn-primary btn-sm" href="{{ route('rfos.create') }}"><i class="fas fa-plus-circle"></i>{{_('Create New RFO')}} </a>
             @endcan
-            <input id="rfosSearch" type="text" class="form-control form-control-sm d-inline-block w-auto" placeholder="Search">
-            <select id="rfosPageSize" class="form-control form-control-sm d-inline-block w-auto">
-                <option value="10">10</option>
-                <option value="20" selected>20</option>
-                <option value="50">50</option>
-                <option value="100">100</option>
-            </select>
         </div>
     </div>
     <!-- /.card-header -->
     <div class="card-body">
-        <table  class="table table-striped js-paginated-table" data-page-size="20" data-page-size-control="#rfosPageSize" data-pager="#rfosPager" data-search="#rfosSearch">
-            <thead>
-                <tr>
-                    <th>No.</th>
-                    <th>Reason For Outage</th>
-                    
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($rfos as $rfo)
-                <tr >
-                    <td>{{++$i}}</td>
-                    <td>{{ $rfo->RFO}}</td>
-                    <td>
-                            @can('department-edit')
-                            <a href="{{ route('rfos.edit',$rfo->id) }}" class="btn btn-sm btn-success" style="padding:0px 2px; color:#fff;" >Edit</a>
-                            @endcan
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <div id="rfosPager" class="mt-2"></div>
+        <div class="table-responsive">
+            <div class="d-flex justify-content-end align-items-center gap-2 mb-2">
+                <div class="input-group input-group-sm" style="width: 170px;">
+                    <div class="input-group-prepend"><span class="input-group-text">Show</span></div>
+                    <select id="rfosPageSize" class="form-select form-select-sm" style="width:auto;">
+                        <option value="10">10</option>
+                        <option value="20" selected>20</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                        <option value="all">All</option>
+                    </select>
+                </div>
+                <div class="input-group input-group-sm" style="width: 220px;">
+                    <input type="text" id="rfosSearch" class="form-control" placeholder="Search rfos">
+                </div>
+            </div>
+            <table  class="table table-hover js-paginated-table" data-page-size="20" data-page-size-control="#rfosPageSize" data-pager="#rfosPager" data-search="#rfosSearch">
+                <thead class="thead=light">
+                    <tr>
+                        <th>No.</th>
+                        <th>Reason For Outage</th>
+                        <th>Actions</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($rfos as $rfo)
+                    <tr >
+                        <td>{{++$i}}</td>
+                        <td>{{ $rfo->RFO}}</td>
+                        <td>
+                                @can('department-edit')
+                                <a href="{{ route('rfos.edit',$rfo->id) }}" class="btn btn-sm btn-outline-success" style="padding:0px 2px;" >
+                                    <i class="fas fa-edit"></i>Edit
+                                </a>
+                                @endcan
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <div id="rfosPager" class="mt-2"></div>
+        </div>
     </div>
     <!-- /.card-body -->
 </div>
