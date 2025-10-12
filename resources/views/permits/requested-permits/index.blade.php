45b947ef58b771fff7f4f1eb909f3921c49292a1
jataz <africajataz@gmail.com>
2025-10-12 21:45:40 +0200

    Changes on search and page sizes

--- resources/views/permits/requested-permits/index.blade.php
+++ resources/views/permits/requested-permits/index.blade.php
@@ -14,45 +14,58 @@ Requested Permits
     <div class="card-header">
         <h3 class="card-title">{{_('Requested Permits')}}</h3>
         <div class="card-tools">
-            <input id="requestedPermitsSearch" type="text" class="form-control form-control-sm d-inline-block w-auto" placeholder="Search">
-            <select id="requestedPermitsPageSize" class="form-control form-control-sm d-inline-block w-auto">
-                <option value="10">10</option>
-                <option value="20" selected>20</option>
-                <option value="50">50</option>
-                <option value="100">100</option>
-            </select>
         </div>
     </div>
     <!-- /.card-header -->
     <div class="card-body">
-        <table  class="table table-striped js-paginated-table" data-page-size="20" data-page-size-control="#requestedPermitsPageSize" data-pager="#requestedPermitsPager" data-search="#requestedPermitsSearch">
-            <thead>
+        <div class="table-responsive">
+            <div class="d-flex justify-content-end align-items-center gap-2 mb-2">
+                <div class="input-group input-group-sm" style="width: 170px;">
+                    <div class="input-group-prepend"><span class="input-group-text">Show</span></div>
+                    <select id="requestedPermitsPageSize" class="form-select form-select-sm" style="width:auto;">
+                        <option value="10">10</option>
+                        <option value="20" selected>20</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                        <option value="all">All</option>
+                    </select>
+                </div>
+                <div class="input-group input-group-sm" style="width: 220px;">
+                    <input type="text" id="requestedPermitsSearch" class="form-control" placeholder="Search Requested Permits">
+                </div>
+            </div>
+            <table  class="table table-hover js-paginated-table" data-page-size="20" data-page-size-control="#requestedPermitsPageSize" data-pager="#requestedPermitsPager" data-search="#requestedPermitsSearch">
+                <thead class="thead-light">
+                    <tr>
+                        <th>ID</th>
+                        <th>Fault ID</th>
+                        <th>PTW Numnber</th>
+                        <th>Customer Name</th>
+                        <th>Date of Request</th>
+                        <th>Technician</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+
                 <tr>
-                    <th>ID</th>
-                    <th>Fault ID</th>
-                    <td>PTW Numnber</td>
-                    <th>Customer Name</th>
-                    <th>Date of Request</th>
-                    <th>Technician</th>
-                    <th>Actions</th>
+                    <td>1</td>
+                    <td>653276</td>
+                    <td>1213313</td>
+                    <th>Zb Bank</th>
+                    <th>05/05/43</th>
+                    <th>Freedom</th>
+                    <td>
+                        <button  class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#showPermitModal">
+                            <i class="fas fa-eye me-1"></i> View
+                        </button>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
 
-            <tr>
-                <td>1</td>
-                <td>653276</td>
-                <td>1213313</td>
-                <th>Zb Bank</th>
-                <th>05/05/43</th>
-                <th>Freedom</th>
-                <td>
-                    <a href="">Delete</a>
-                </td>
-            </tr>
-
-            </tbody> 
-        </table>
+                </tbody> 
+            </table>
+            <div id="requestedPermitsPager" class="mt-2"></div>
+        </div>
     </div>
     <!-- /.card-body -->
 </div>
@@ -61,5 +74,5 @@ Requested Permits
 @endsection
 @section('scripts')
     @include('partials.scripts')
-<div id="requestedPermitsPager" class="mt-2"></div>
+
 @endsection
